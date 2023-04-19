94ad4e7521e4436b7d4633017f5ece917f39cc8d
cmrotruck <crotruck@gmail.com>
2023-04-18 20:05:30 -0400

    added ability to pull from last 10 draws and display to page.

--- src/__test__/powerball.test.js
+++ src/__test__/powerball.test.js
@@ -1,8 +1,14 @@
-import { GetPowerballDrawings } from "../lib/pb";
+import { GetPowerballDrawings, GetLast10Draws } from "../lib/pb";
 //const { GetPowerballDrawings } = require("../lib/powerball");
 
 test("Gets all draws from api", async () => {
   var results = await GetPowerballDrawings();
-  console.log(results);
+  //console.log(results);
+  expect(results.status).toBe("success");
+});
+
+test("Gets the last 10 draws from api", async () => {
+  var results = await GetLast10Draws();
+  //console.log(results);
   expect(results.status).toBe("success");
 });
--- src/lib/pb.js
+++ src/lib/pb.js
@@ -13,6 +13,38 @@ function GetPowerballDrawings() {
     .catch((err) => console.error(err));
 }
 
+function GetLast10Draws() {
+  //gets the last 10 draws from powerball api
+  const options = {
+    method: "Get",
+    headers: {
+      "X-RapidAPI-Key": "7a3bbb4a75msh027520d2b0cd076p1e2befjsn318461451384",
+      "X-RapidAPI-Host": "powerball.p.rapidapi.com",
+    },
+  };
+
+  return fetch("https://powerball.p.rapidapi.com/Latest10", options)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+}
+
+function GetLatestDrawing() {
+  //gets the last 10 draws from powerball api
+  const options = {
+    method: "Get",
+    headers: {
+      "X-RapidAPI-Key": "7a3bbb4a75msh027520d2b0cd076p1e2befjsn318461451384",
+      "X-RapidAPI-Host": "powerball.p.rapidapi.com",
+    },
+  };
+
+  return fetch("https://powerball.p.rapidapi.com/latest", options)
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+}
+
 module.exports = {
   GetPowerballDrawings,
+  GetLast10Draws,
+  GetLatestDrawing,
 };
